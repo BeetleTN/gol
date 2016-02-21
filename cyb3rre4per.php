8e51ed4204f1f47900e135685dcf5f0e95418b23
BeetleTN <beetledesignagency@gmail.com>
2016-02-21 05:08:03 +0100

    Added files via upload

---
+++ cyb3rre4per.php
@@ -0,0 +1,349 @@
+
+<!DOCTYPE html>
+<html >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="description" content=" Operation Tunisia refers to the actions by internet group Anonymous during the Tunisian revolution."/>
+<meta name="keywords" content="Anonymous, Opération Tunisie, Ammar404, Operation Tunisia, OpTunisia, Anonymous TN, Revolution"/>
+<meta name="author" content="Anonymous">
+<meta name="robots" content="index, follow">
+<title></title> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<link rel='stylesheet prefetch' href='bootstrap.css'>
+<link rel='stylesheet prefetch' href=' ../Download/source-code-pro-gh-pages/source-code-pro-gh-pages/source-code-pro.css'>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Code+Pro">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css ">
+<style> body {
+background: #0d0d0d;
+color: #Ffffff;
+font-family: 'Source Code Pro', sans-serif;
+font-weight: 300;
+}h1, .h2, .h3, h1, h2, h3 {
+ font-weight: 200;
+}
+
+.header h1 {
+font-size: 50px;
+text-align:center;
+}
+
+.header h1 span {
+color: #cd201f ;
+}
+
+.header p {
+text-align:center;
+}
+a,a:hover{
+color: #cd201f ;
+}
+.loading { background: #000000; position: fixed; top: 0; bottom: 0; right: 0; left: 0; z-index: 9999; width: 100%; height: 100%; text-align: center; padding: 6px 17px; }
+.loading .snip {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -moz-transform: translateX(-50%) translateY(-50%);
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+}
+
+
+ ng{
+white-space: nowrap;
+overflow-x: auto;
+-webkit-overflow-scrolling: touch;
+-ms-overflow-style: -ms-autohiding-scrollbar;
+width: 100%;
+}
+.example-two-header {
+}
+
+nabv {
+color : #fff;
+padding: 13px 0px;
+}
+hhh:not(:last-child)::after {
+content: '/';
+padding-left:9px;
+padding-right:8px;
+text-decoration: none;
+}
+
+hh {
+display: inline-block;
+vertical-align: middle;
+}
+
+
+
+
+
+
+
+.news-grid .row .news-card {
+    background: #000000;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+height: 550px;
+  padding: 0px;
+}
+.news-grid .row .news-card .overlay {
+    width: 100%;
+    height: 100%;
+
+background: -moz-linear-gradient(top, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 35%, rgba(0,0,0,0.8) 100%);
+
+background: -webkit-linear-gradient(top, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 35%,rgba(0,0,0,0.8) 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 35%,rgba(0,0,0,0.8) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00000000', endColorstr='#80000000',GradientType=0 ); /* IE6-9 */
+    cursor: pointer;
+    -webkit-transition: all .4s ease;
+    -moz-transition: all .4s ease;
+    -ms-transition: all .4s ease;
+    -o-transition: all .4s ease;
+    transition: all .4s ease;
+}
+.news-grid .row .news-card:hover .overlay {
+    ohpacity: 0;
+}
+.news-grid .row .news-card .meta-post {
+
+position:absolute;
+    color: #FFF;
+bottom:0;
+left:10px;
+  text-align: left;
+    width: 100%;
+height: auto;
+border-left: 2px solid #cd201f;
+    padding: 5px 20px;
+padding-right: 10px;
+margin-bottom:15px;
+} 
+
+
+
+.news-grid .row .news-cayrd:hover .meta-post {
+ 	 -webkit-transform: translate3d(0, 0, 0);			 transform: translate3d(0, 0, 0);
+}
+.news-grid .row .news-card .meta-post h1 {
+    font-weight: normal;
+    font-size: 25px;
+}
+
+.news-grid .row .news-card .meta-post p {
+    font-size: 13px;
+}
+.news-grid .row .news-card .meta-post .info {
+    color: #B7B7B7;
+    font-weight: bold;
+}
+
+.btn-readmore {
+    background: #cd201f ;
+    border: 2px solid #cd201f ;
+    padding: 7px 20px;
+margin:20px 0px;
+    color: #FFF;
+    font-weight: bold;
+    border-radius: 20px;
+    text-transform: uppercase;
+  -webkit-transition: all .35s linear;
+-moz-transition: all .35s linear;
+-ms-transition: all .35s linear;
+-o-transition: all .35s linear;
+transition: all .35s linear;
+}
+
+
+
+
+
+
+
+
+ .topnav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+white-space: nowrap;
+overflow-x: auto;
+-webkit-overflow-scrolling: touch;
+-ms-overflow-style: -ms-autohiding-scrollbar;
+width: 100%;
+
+display: inline-block;
+vertical-align: middle;
+}
+
+.topnav ul li {
+    float: none;
+display: inline-block;
+vertical-align: middle;
+}
+
+.topnav ul li a {
+    display: inline-block;
+    color: white;
+    text-align: center;
+    padding: 10px 8px;
+    text-decoration: none;
+}
+
+.topnav ul li a:hover {
+    background-color: #111;
+}
+
+.topnav ul li:not(:last-child)::after {
+content: '/';
+}
+
+
+
+
+
+
+.loading {display: none; background: #000000; position: fixed; top: 0; bottom: 0; right: 0; left: 0; z-index: 9999; width: 100%; height: 100%; text-align: center; padding: 6px 17px; } .loading .snip { position: absolute; top: 50%; left: 50%; -moz-transform: translateX(-50%) translateY(-50%); -webkit-transform: translateX(-50%) translateY(-50%); transform: translateX(-50%) translateY(-50%); }
+
+
+ </style>
+</head>
+<body>
+<div class="loading">
+   <div class="snip">
+      <p>Loading...</p>
+   </div>
+</div>
+<header class=' container-fluid example-two-header'>
+<nav class="topnav">
+ <ul>
+   <li> <a href="#/home/">Home</a> </li>
+   <li> <a href="#/blog/">Blog</a> </li>
+   <li> <a href="#/projects/">Projects</a> </li>
+   <li> <a href="#/about/">About</a> </li>
+   <li> <a href="#/contact/">Contact</a> </li>
+   <li> <a href="#">Github</a> </li>
+   <li> <a href="#">Twitter</a> </li>
+ </ul>
+</nav>
+</header>
+
+
+<div class="header">
+<div class="container-fluid">
+<h1>Cyber {Reaper}</h1>
+<br><br>
+<p> Tunix is a small development agency based in Tunisia <p id="cont"></p></p>
+</div>
+</div>
+
+
+
+
+
+<br><br><br>
+
+
+
+<div class="content">
+
+</div>
+
+
+
+
+
+<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+
+
+
+<script src="jquery.routes.js" type="text/javascript"></script>
+
+<script>
+
+var pages = 'templates/pages/';
+var sitename = 'Cyb3r Re4per | ';
+
+
+//var pageurl = window.location.hash;
+var pageurl = $('.topnav ul li a').attr('href');
+//var pageurl = pageurl.replace(/\/$/, '');
+
+var pageurl = pageurl.substr(2);
+var pageurl = pageurl.slice(0, -1);
+
+
+
+
+
+
+$(window).load(function(){
+  if(window.location.hash) {} else {
+      window.location.hash = '/home/';
+  }
+});
+$.routes.add('/home/', function() {
+  document.title = sitename + 'Home';
+  $(".loading").fadeIn(750);
+  alert('URL : ' + pages + 'home.html');
+  $("#content").load(pages + 'home.html');
+  $(".loading").fadeOut(750);
+});
+$.routes.add('/about/', function() {
+
+
+
+$(".content").load("home.html", function(responseTxt, statusTxt, xhr){
+            if(statusTxt == "success")
+                alert("External content loaded successfully!");
+            if(statusTxt == "error")
+                alert("Error: " + xhr.status + ": " + xhr.statusText);
+        });
+
+
+
+
+
+
+  $(".content").load(pages+'about.html');
+  document.title = sitename + 'About';
+  $(".loading").fadeIn(750);
+  alert('URL : ' + pages + 'about.html');
+  $("#content").load(pages + 'about.html');
+  $(".loading").fadeOut(750);
+//  $(".content").html( document.location.protocol );
+});
+$.routes.add('/blog/', function() {
+  document.title = sitename + 'Blog';
+  $(".loading").fadeIn(750);
+  alert('URL : ' + pages + 'blog.html');
+  $("#content").load(pages + 'blog.html');
+  $(".loading").fadeOut(750);
+});
+$.routes.add('/projects/', function() {
+  document.title = sitename + 'Projects';
+  $(".loading").fadeIn(750);
+  alert('URL : ' + pages + 'projects.html');
+  $("#content").load(pages + 'projects.html');
+  $(".loading").fadeOut(750);
+});
+$.routes.add('/contact/', function() {
+  document.title = sitename + 'Contact';
+  $(".loading").fadeIn(750);
+  alert('URL : ' + pages + 'contact.html');
+  $("#content").load(pages + 'contact.html');
+  $(".loading").fadeOut(750);
+});
+$.routes.add('/Ab2_o', function() {
+   window.location.href = 'newPage.html';
+});
+$.routes.add('', function() {
+  document.title = sitename + '404 Not Found';
+  $(".loading").fadeIn(750);
+  alert('URL : ' + pages + '404.html');
+  $("#content").load(pages + '404.html');
+  $(".loading").fadeOut(750);
+});
+</script>
+</body></html> 
